34a5798f2055a59ac939c038b341acafb6b30760
김미희 <opalgml@naver.com>
2020-07-07 20:17:17 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
