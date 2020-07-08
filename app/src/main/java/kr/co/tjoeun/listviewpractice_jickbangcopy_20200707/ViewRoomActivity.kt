c8c543d98d4bf313de2492b1e7297d1e0384088f
김미희 <opalgml@naver.com>
2020-07-08 21:18:10 +0900

    방 상세 화면액티비티로 이동 기능 수정, 앱 테마 색상 변경

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/ViewRoomActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/ViewRoomActivity.kt
@@ -2,14 +2,19 @@ package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_view_room.*
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas.Room
 import kotlin.math.floor
+import kotlin.math.log
 
 class ViewRoomActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room)
+
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents()
@@ -21,10 +26,11 @@ class ViewRoomActivity : BaseActivity() {
     {
         val receivedRoom = intent.getSerializableExtra("room") as Room
 
-        priceTxt.text = receivedRoom.getFormattedFloor()
+        priceTxt.text = receivedRoom.getFormattedPrice()
         descTxt.text = receivedRoom.description
         addressTxt.text = receivedRoom.address
         floorTxt.text = receivedRoom.getFormattedFloor()
+        Log.d("TAG", "price : " + priceTxt.text + "  descTxt :" + receivedRoom.description)
 
     }
 
